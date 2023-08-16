d51548b4baf8a6035d66292e87cc91e43661f85d
nuyos <id2ana@naver.com>
2023-08-16 13:23:24 +0900

    초기 커밋.

---
+++ WPF-LoginForm/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using WPF_LoginForm.Views;
+
+namespace WPF_LoginForm
+{
+    /// <summary>
+    /// App.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class App : Application
+    {
+        protected void ApplicationStart(object sender, StartupEventArgs e)
+        {
+            var loginView = new LoginView();
+            loginView.Show();
+            loginView.IsVisibleChanged += (s, ev) =>
+            {
+                if (loginView.IsVisible == false && loginView.IsLoaded)
+                {
+                    var mainView = new MainView();
+                    mainView.Show();
+                    loginView.Close();
+                }
+            };
+        }
+    }
+}
---
+++ WPF-LoginForm/ViewModel/ViewModelCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPF_LoginForm.ViewModel
+{
+    internal class ViewModelCommand : ICommand
+    {
+        private readonly Action<object> _execteAction;
+        private readonly Predicate<object> _canExecuteAction;
+
+        public ViewModelCommand(Action<object> execteAction)
+        {
+            _execteAction = execteAction;
+            _canExecuteAction = null;
+        }
+        public ViewModelCommand(Action<object> execteAction, Predicate<object> canExecuteAction)
+        {
+            _execteAction = execteAction;
+            _canExecuteAction = canExecuteAction;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecuteAction == null?true : _canExecuteAction(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execteAction(parameter);
+        }
+    }
+}
